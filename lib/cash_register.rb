3a08326c2a28e373cf136428511a17a705f2cb94
Mark Hernandez <m.hern1415@gmail.com>
2019-05-17 20:40:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,8 +7,19 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, )
-
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
 
 
 
